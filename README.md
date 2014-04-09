b1dbb7e7f74920b0427eb2ca5e8519748556599a
Aaron Hurt <ahurt@anbcs.com>
2014-04-09 09:21:37 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+homebrew-freeradius3
+====================
+
+You can use this formula to install the latest [FreeRADIUS][] 3.x release.
+
+How do I install these formulae?
+--------------------------------
+Just `brew tap leprechau/homebrew-freeradius3` and then `brew install <formula>`.
+
+If the formula conflicts with one from mxcl/master or another tap, you can `brew install leprechau/homebrew-freeradius3/<formula>`.
+
+You can also install via URL:
+
+
+    brew install https://raw.github.com/leprechau/homebrew-freeradius3/master/<formula>.rb
+
+
+Docs
+----
+`brew help`, `man brew`, or the Homebrew [wiki][].
+
+
+[FreeRADIUS]:https://github.com/FreeRADIUS/freeradius-server
+[wiki]:http://wiki.github.com/mxcl/homebrew
---
+++ freeradius-server3.rb
@@ -0,0 +1,38 @@
+require 'formula'
+
+class FreeradiusServer3 < Formula
+  homepage 'http://freeradius.org/'
+  url 'ftp://ftp.freeradius.org/pub/freeradius/freeradius-server-3.0.2.tar.gz'
+  sha1 '27e908e245470e96e5a5fea0f00c09901b0d7fb5'
+
+  # needs talloc and openssl
+  depends_on 'talloc'
+  depends_on 'openssl'
+
+  # optional depends for modules
+  depends_on 'mysql'      => :optional # rlm_sql
+  depends_on 'postgresql' => :optional # rlm_sql
+  depends_on 'sqlite'     => :optional # rlm_sql
+  depends_on 'unixodbc'   => :optional # rlm_sql
+  depends_on 'libiodbc'   => :optional # rlm_sql
+  depends_on 'freetds'    => :optional # rlm_sql
+  depends_on 'gdbm'       => :optional # rlm_ippool
+  depends_on 'json-c'     => :optional # rlm_rest
+  depends_on 'redis'      => :optional # rlm_redis, rlm_rediswho
+  depends_on 'ykclient'   => :optional # rlm_yubikey
+
+  def install
+    ENV.deparallelize
+
+    system "./configure", "--prefix=#{prefix}",
+                          "--without-rlm_eap_ikev2",
+                          "--without-rlm_eap_tnc",
+                          "--without-rlm_sql_db2",
+                          "--without-rlm_sql_firebird",
+                          "--without-rlm_sql_oracle",
+                          "--without-rlm_securid"
+    system "make"
+    system "make install"
+  end
+end
+
